4f97f960f77ed24fed71b552a323079a8749f0b2
Anders Kanth <kaan18kc@student.ju.se>
2019-09-19 13:32:29 +0200

    har skapat upp formen, laggt upp info om anläggningarna. skapat js fil, contactController etc.

---
+++ BolindersBil.web/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BolindersBil.web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult SendForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ BolindersBil.web/ViewModels/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BolindersBil.web.ViewModels
+{
+    public class ContactViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Fyll i ditt namn.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Fyll i din email.")]
+        public string Email { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Fyll i ett ämne.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Skriv ditt meddelande.")]
+        public string Message { get; set; }
+
+        public string DealerShipMail { get; set; }
+
+    }
+}
