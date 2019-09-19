50097ab1bc5ef9379ae22014b0369cbbceea9518
Alexander Marin <maal18en@student.ju.se>
2019-09-19 13:22:18 +0200

    r

--- BolindersBil.web/Controllers/AdminController.cs
+++ BolindersBil.web/Controllers/AdminController.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BolindersBil.web.DB;
+using BolindersBil.web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BolindersBil.web.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly BolindersBilDatabaseContext _context;
+        public AdminController(BolindersBilDatabaseContext context)
+        {
+            _context = context;
+
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Vehicle car)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Vehicle.Add(car);
+                await _context.SaveChangesAsync();
+
+            }
+
             return View();
         }
     }
