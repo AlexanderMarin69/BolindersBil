875364f707531419957cebdf62b49d61b960c3d1
Berbecut <dafl18ye@student.ju.se>
2019-10-09 00:15:23 +0200

    Florin_Admin_Page

---
+++ BolindersBil.web/ViewModels/EditVehicleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BolindersBil.web.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BolindersBil.web.ViewModels
+{
+    public class EditVehicleViewModel
+    {
+        public Vehicle Vehicle { get; set; }
+        //public List<SelectListItem> Categories { get; set; }
+
+        public IEnumerable<SelectListItem> Brands { get; set; }
+        public IEnumerable<SelectListItem> Dealerships { get; internal set; }
+        public List<FileUpload> FileUpload { get; set; }
+
+
+
+        //public Vehicle Vehicle { get; set; }
+        //public List<SelectListItem> Brands { get; set; }
+        //public List<SelectListItem> DealerShips { get; set; }
+        //public List<FileUpload> FileUpload { get; set; }
+    }
+}
