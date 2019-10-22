66cb06c81b4fe6a35d4505b210286489b1d9a8b3
Elvin Causevic <cael18wh@student.ju.se>
2019-10-22 13:31:21 +0200

    Fixat bara design kvar

--- BolindersBil.web/Controllers/ContactController.cs
+++ BolindersBil.web/Controllers/ContactController.cs
@@ -54,22 +54,24 @@ namespace BolindersBil.web.Controllers
 
         
         [HttpPost]
-        public IActionResult Link(ShareViewModel model)
+        public IActionResult Link(ShareViewModel vm)
         {
 
             var domain = HttpContext.Request.Host.Value; //localhos:1234
-            var protocol = HttpContext.Request.Scheme; //https
-            var url = Url.Action("CarPage", "filter", new  { id = model.CarId } );
+            var protocol = HttpContext.Request.Scheme + "://"; //https
+            var url = Url.RouteUrl("CarDetailsPage", new { make = vm.Brand, model = vm.Model, registrationNumber = vm.RegistrationNumber, id = vm.CarId });
+            var FullUrl = protocol + domain + url;
 
             var msg = new MimeMessage();
 
             msg.From.Add(new MailboxAddress("kontakt@bolindersbil.se"));
-            msg.To.Add(new MailboxAddress(model.Email));
+            msg.To.Add(new MailboxAddress(vm.Email));
 
             msg.Subject = "En vän delat en bil från BolindersBil";
             msg.Body = new TextPart("Html")
             {
-                Text = "<strong>Kolla in denna schyssta bilen som finns i vårt lager</strong>" + "<br/>" + protocol + "//:" + domain + url ,ContentTransferEncoding = ContentEncoding.QuotedPrintable
+                Text = "<strong>Kolla in denna schyssta bilen som finns i vårt lager</strong>" + "<br/>" +  $"<a href='{FullUrl}' target='_blank'>{FullUrl}</a>",
+                ContentTransferEncoding = ContentEncoding.QuotedPrintable
             };
 
 
@@ -80,7 +82,7 @@ namespace BolindersBil.web.Controllers
             client.Disconnect(true);
 
  
-            var url2 = Url.RouteUrl("CarDetailsPage", new { make = model.Brand, model = model.Model, registrationNumber = model.RegistrationNumber, id = model.CarId });
+            var url2 = Url.RouteUrl("CarDetailsPage", new { make = vm.Brand, model = vm.Model, registrationNumber = vm.RegistrationNumber, id = vm.CarId });
             return Redirect(url2);
         }
 
